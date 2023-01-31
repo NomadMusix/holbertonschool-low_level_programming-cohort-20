733c14e818d28d9eda3eef015fc94bf708cb5384
Alonzo Johnson <nomadmusix@gmail.com>
2023-01-30 20:32:28 -0800

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@ nclude <stdio.h>
  */
 int main(void)
         {
-		puts("\"Programming is like building a multilingual puzzle"0;
-				return (0)
-				}			
+	puts("\"Programming is like building a multilingual puzzle"0;
+	return (0)
+	}			
