5e352fcd589413de9fe82d6ea5e1c676e3db62e3
Alonzo Johnson <nomadmusix@gmail.com>
2023-02-06 17:17:44 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_line - prints a straight line in terminal
+ * @n: integer to be checked for line length
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i + 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
