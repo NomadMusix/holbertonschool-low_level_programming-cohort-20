e4d1fb49cf7f2efad836fb5bd43e37f46b11ed6d
Alonzo Johnson <nomadmusix@gmail.com>
2023-02-06 17:22:27 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal line
+ * @n: interger to be checked
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (i <= 0)
+		_putchar('\n')
+}
