712fc52104101b46d85d80527cf832a77eae8ea2
Alonzo Johnson <nomadmusix@gmail.com>
2023-02-11 12:54:40 -0800

    Commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ *
+ * function concatenates two strings
+ * dest: string to be appended
+ * src: appended from
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = 0, b = 0;
+
+	while (dest[a])
+	{
+		a++;
+	}
+	
+	while (b < n src[b])
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	
+	dest[a] = '\0';
+
+	return (dest);
+}
