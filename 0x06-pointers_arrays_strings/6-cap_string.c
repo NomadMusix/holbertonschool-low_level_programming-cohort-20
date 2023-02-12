d52cf1a47f54812041837d81146480dc767685f7
Alonzo Johnson <nomadmusix@gmail.com>
2023-02-12 15:42:47 -0800

    Commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * cap_string: capitalizes all words of a string
+ * s - string to cap
+ *
+ * Return pointer to string
+ */
+char *cap_string(char *s)
+{
+	int i;
+	int prev_space = 1;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (pre_space && (s[i] >= 'a' && s[i] <= 'z'))
+		{
+			s[i] -= 32;
+		}
+
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ','
+				|| s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?'
+				|| s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{'
+				|| s[i] == '}')
+		{
+			prev_space = 1;
+		}
+		else
+		{
+			prev_space = 0;
+		}
+	}
+
+	return (s);
+}
