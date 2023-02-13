0c0e97f9a73426d181bf0c57638a7833456d8f8a
Alonzo Johnson <nomadmusix@gmail.com>
2023-02-12 17:34:24 -0800

    Commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * leet - encodes a string into 1337
+ * s: string to encode
+ *
+ * Return: encode string
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char letters[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int numbers[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
