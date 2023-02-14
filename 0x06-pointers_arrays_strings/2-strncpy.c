42f717185cee204a17ac072376db59c74b8bc4c0
Alonzo Johnson <nomadmusix@gmail.com>
2023-02-13 20:22:37 -0800

    Commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,11 @@
 #include "holberton.h"
 /**
- * _strncpy, function that copies a string
+ * _strncpy - function that copies a string.
+ * @dest: destination string
+ * @src: source string
+ * @n:maximum number of characters to be copied from source
  *
+ * Return: pointer to destination string.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
@@ -11,6 +15,6 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[a] = src[a];
 	for ( ; a < n; a++)
 		dest[a] = '\0';
-	
+
 	return (dest);
 }
