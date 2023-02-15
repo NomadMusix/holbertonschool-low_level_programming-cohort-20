b4e7f9917ad01e11bf57603a2f4fbd8a4dbee691
Alonzo Johnson <nomadmusix@gmail.com>
2023-02-15 14:00:31 -0800

    commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - program prints the sign of n
+ *
+ * @n: Input from user or program
+ *
+ * Return: 0 success
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+);
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar(' - ');
+	return (-1);
+}
