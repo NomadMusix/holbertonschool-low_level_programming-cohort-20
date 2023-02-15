16fb7742e295561897427735503268c44555baff
Alonzo Johnson <nomadmusix@gmail.com>
2023-02-15 12:19:08 -0800

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - displays zero through nine
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n');
+	return (0);
+}
