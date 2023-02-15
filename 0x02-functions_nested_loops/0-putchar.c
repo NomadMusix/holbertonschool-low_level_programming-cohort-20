824a5a9492f13673ce7cdf710034b320bc0e16b6
Alonzo Johnson <nomadmusix@gmail.com>
2023-02-15 13:18:36 -0800

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * prints_alphabet - prints the alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char *s = "_putchar\n";
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		putchar(s[i]);
+	}
+	return (0);
+}
